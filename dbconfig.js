90249b76d75bc27e9b738d36feb9619ff1306b94
Shamim Ehsan <ehsanhrid@gmail.com>
2018-06-04 12:18:15 +0600

    Added connection with MongoDb

---
+++ dbconfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dburl: 'mongodb://ehsan:qwerty@ds119080.mlab.com:19080/ehsandb'
+};
--- server.js
+++ server.js
@@ -6,5 +6,13 @@ app.use(bodyparser.raw({extended: true}));
 const PORT = 3000;
 const HOST = '0.0.0.0'
 
-app.listen(PORT, HOST);
-console.log("server is running");
+let db;
+const { MongoClient } = require('mongodb');
+const collectionName = 'mobileTest'
+const { dburl } = require('./dbconfig');
+MongoClient.connect( dburl ,(err, client)=>{
+	if(err) return console.log(err);
+	db = client.db('ehsandb');
+	app.listen(PORT, HOST);
+	console.log("Server is running");
+})
