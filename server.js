b7ea7cdb66d611c3b47248f40e72bdd583e8c82f
Shamim Ehsan <ehsanhrid@gmail.com>
2018-06-04 12:57:46 +0600

    Added POST/values route  for single key value pair

--- server.js
+++ server.js
@@ -8,11 +8,28 @@ const HOST = '0.0.0.0'
 
 let db;
 const { MongoClient } = require('mongodb');
-const collectionName = 'mobileTest'
+const { ObjectID } = require('mongodb');
+const collectionName = 'pathaoTest'
 const { dburl } = require('./dbconfig');
 MongoClient.connect( dburl ,(err, client)=>{
 	if(err) return console.log(err);
 	db = client.db('ehsandb');
 	app.listen(PORT, HOST);
 	console.log("Server is running");
+})
+
+var requestTime = function (req, res, next) {
+  req.body.createdAt = Date.now();
+  console.log(Date.now());
+  next()
+}
+
+app.use(requestTime);
+
+app.post('/values', (req,res) =>{
+	db.collection(collectionName).save(req.body, (err, result)=>{
+		if(err) return console.log(err);
+		console.log("saved to database");
+		res.status(201).send(req.body); 
+	})
 })
